53e1327db60a610771a50f2f79507cf728f62b36
EduFalbel <du.falbel@gmail.com>
2022-12-14 17:07:25 +0100

    Refactor methods

--- src/seim/model_estimation.r
+++ src/seim/model_estimation.r
@@ -6,83 +6,94 @@ library(readr)
 library(MASS)
 # library(rgdal)
 
-
+options(warn=-1)
 set.seed(42)
+
+# Default weights matrix is row-normalized without distance decay based on queen contiguity neighborhood
+default_wm_options <- function() {
+    wm_options <- c("contiguity", TRUE, "standard", "W")
+    names(wm_options) <- c("neighbour", "queen/k", "weight", "style")
+    return(wm_options)
+}
 
 create_weights_matrix <- function(options, node_data) {
     if (as.character(options[["neighbour"]]) == "contiguity") {
         neighbours <- spdep::poly2nb(node_data, queen = options[["queen/k"]])
     } else if (as.character(options[["neighbour"]] == "knn")) {
-        neighbours <- knn2nb(knearneigh(st_centroid(node_data), k=as.numeric(options[["queen/k"]]))) 
+        neighbours <- knn2nb(knearneigh(st_centroid(node_data), k=as.numeric(options[["queen/k"]])))
     }
 
     if (as.character(options[["weight"]] == "dist_decay")) {
         weights_matrix <- listw2mat(nb2listwdist(neighbours, st_centroid(node_data), type = as.character(options[["type"]]), style = as.character(options[["style"]])))
-    
+
     } else if (as.character(options[["weight"]] == "standard")) {
         weights_matrix <- nb2mat(neighbours, style = as.character(options[["style"]]), zero.policy = TRUE)#FALSE)
-    
+
     }
 
     return(weights_matrix)
 }
 
-read_data <- function(node_path, pair_path, wm_options) {
+create_and_write_weights_matrix <- function(node_path, data_save_path = "/tmp/", wm_options = NULL, ...) {
+    
+    node_data <- sf::st_read(node_path)
+    if (is.null(wm_options)) {
+        wm_options <- default_wm_options()
+    }
+    weights_matrix <- create_weights_matrix(wm_options, node_data)
+    write.matrix(weights_matrix, file = paste0(data_save_path, "weights.txt"))
+    return(weights_matrix)
+}
+
+read_data <- function(node_path, pair_path) {
     node_data <- sf::st_read(node_path)
     pair_data <- read_csv(pair_path)
 
-    weights_matrix <- create_weights_matrix(wm_options, node_data)
-
-    # print(class(node_data))
-
-    return(list(node_data, pair_data, weights_matrix))
+    return(list(node_data, pair_data))
 }
 
-write_data <- function(node_data, pair_data, weights_matrix, data_save_dir) {
-    write.csv(st_drop_geometry(node_data), file = paste0(data_save_dir, "node.csv"), row.names = FALSE)
-    write.csv(pair_data, file = paste0(data_save_dir, "pair.csv"), row.names = FALSE)
-    write.matrix(weights_matrix, file = paste0(data_save_dir, "weights.txt"))
+write_data <- function(node_data, pair_data, data_save_path) {
+    write.csv(st_drop_geometry(node_data), file = paste0(data_save_path, "node.csv"), row.names = FALSE)
+    write.csv(pair_data, file = paste0(data_save_path, "pair.csv"), row.names = FALSE)
 }
 
-read_and_write_data <- function(node_path, pair_path, wm_options, data_save_dir) {
-    data <- read_data(node_path, pair_path, wm_options)
-    write_data(data[[1]], data[[2]], data[[3]], data_save_dir)
+read_and_write_data <- function(node_path, pair_path, data_save_path) {
+    data <- read_data(node_path, pair_path)
+    write_data(data[[1]], data[[2]], data_save_path)
     return(data)
 }
 
-
-
 create_cntrl <- function(cntrl_type) {
-    if (cntrl_type == 'SLA') {
+    if (cntrl_type == "SLA") {
        cntrl <- spflow_control(
         sdm_variables = "none"
        )
     }
-    if (cntrl_type == 'Aspatial') {
+    if (cntrl_type == "Aspatial") {
        cntrl <- spflow_control(
         sdm_variables = "none",
         model = "model_1"
        )
     }
-    if (cntrl_type == 'SLX') {
+    if (cntrl_type == "SLX") {
        cntrl <- spflow_control(
         sdm_variables = "all",
         model = "model_1"
        )
     }
-    if (cntrl_type == 'SLA') {
+    if (cntrl_type == "SDM") {
        cntrl <- spflow_control(
         sdm_variables = "all"
        )
     }
-    
+
     return(cntrl)
 }
 
-estimate_model_params <- function(node_path, pair_path, data_save_path, region_name, dependent_var, wm_options, node_key = "ID", pair_orig_key = "ID_ORIG", pair_dest_key = "ID_DEST", form = NULL, cntrl_type = NULL) {
+estimate_model_params <- function(node_path, pair_path, dependent_var, data_save_path, region_name = "region", node_key = "ID", pair_orig_key = "ID_ORIG", pair_dest_key = "ID_DEST", cntrl_type = "SLA", wm_options = NULL, form = NULL) {
 
 
-    if(is.null(form)) {
+    if (is.null(form)) {
         form <- as.formula(paste0(dependent_var, " ~ ."))
     } else {
         # http://www.cookbook-r.com/Formulas/Creating_a_formula_from_a_string/
@@ -91,21 +102,18 @@ estimate_model_params <- function(node_path, pair_path, data_save_path, region_n
 
     print(form)
 
-    if(is.null(cntrl_type)) {
-        # Defaults to SLA model
-        cntrl <- spflow_control(
-            sdm_variables = "none"
-        )
-    } else {
-        cntrl <- create_cntrl(cntrl_type)
+    if (is.null(wm_options)) {
+        wm_options <- default_wm_options()
     }
 
-    data <- read_and_write_data(node_path, pair_path, wm_options, data_save_path)
-    print("Out")
+
+    cntrl <- create_cntrl(cntrl_type)
+
+    data <- read_data(node_path, pair_path)
     node <- data[[1]]
     pair <- data[[2]]
-    weights_matrix <- data[[3]]
 
+    weights_matrix <- create_weights_matrix(wm_options, node)
 
     print(class(node))
     print(class(pair))
@@ -133,7 +141,7 @@ estimate_model_params <- function(node_path, pair_path, data_save_path, region_n
 
     model <- spflow(
         flow_formula = form,
-        sp_multi_network =  multi_net,
+        sp_multi_network = multi_net,
         flow_control = cntrl
     )
 
